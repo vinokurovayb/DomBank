f88a26cf66afd7a2a223f8954e81cfcf8e526db3
vinokurovayb <vinich1@mail.ru>
2019-02-10 13:28:30 +0300

    Примерно navbar
    Макет index с навбаром на бутстрапе

---
+++ templates/footer.php
@@ -0,0 +1,15 @@
+<!-- здесь будет футер -->
+
+        <div class="footer">
+            <div class="footer-about-info">
+                <a href="index.php" class="logo"></a>
+                <a href="index.php" class="company-name">АО МКБ "ДОМ-БАНК"</a>
+            </div>
+        </div>
+    </div>
+    <script src="javascript/jquery-3.3.1.js"></script>
+    <script src="javascript/popper.min.js"></script>
+    <script src="javascript/bootstrap.min.js"></script>
+    <script src="javascript/main.js"></script>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $title ?></title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class="header-container-logo">
+                <a href="index.php" class="logo"></a>
+                <a href="index.php" class="company-name">АО МКБ "ДОМ-БАНК"</a>
+            </div>
+            <div class="header-navbar">
+                <nav class="navbar navbar-expand-sm navbar-light bg-light">
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">О банке</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">История</a>
+                            <a class="dropdown-item" href="#">Органы управления</a>
+                            <a class="dropdown-item" href="#">Реквизиты</a>
+                            <a class="dropdown-item" href="#">Лицензии</a>
+                            <a class="dropdown-item" href="#">Надзорные органы</a>
+                            <a class="dropdown-item" href="#">Финансовая отчетность</a>
+                            <a class="dropdown-item" href="#">Официальные сведения</a>
+                            <a class="dropdown-item" href="#">Система страховых вкладов</a>
+                            <a class="dropdown-item" href="#">Вакансии</a>
+                            <a class="dropdown-item" href="#">FATCA и CRS</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Частным лицам</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Вклады</a>
+                            <a class="dropdown-item" href="#">Кредиты</a>
+                            <a class="dropdown-item" href="#">Переводы</a>
+                            <a class="dropdown-item" href="#">Карты</a>
+                            <a class="dropdown-item" href="#">О безопасности в интернете..</a>
+                            <a class="dropdown-item" href="#">Обслужтвание счетов</a>
+                            <a class="dropdown-item" href="#">Операции с валютой</a>
+                            <a class="dropdown-item" href="#">Тарифы</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Организациям</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">РКО</a>
+                            <a class="dropdown-item" href="#">Кредитование</a>
+                            <a class="dropdown-item" href="#">Депозиты и векселя</a>
+                            <a class="dropdown-item" href="#">Документарные операции</a>
+                            <a class="dropdown-item" href="#">Банк-Клиент</a>
+                            <a class="dropdown-item" href="#">Карты</a>
+                            <a class="dropdown-item" href="#">Тарифы</a>
+                            <a class="dropdown-item" href="#">Торговый эквайринг</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Банкам</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Корреспондентские отношения</a>
+                            <a class="dropdown-item" href="#">Денежный рынок</a>
+                            <a class="dropdown-item" href="#">Инкассация</a>
+                            <a class="dropdown-item" href="#">Тарифы</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Сеть обслуживания</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">ЦО</a>
+                            <a class="dropdown-item" href="#">Опер.кассы</a>
+                            <a class="dropdown-item" href="#">Банкоматы</a>
+                            <a class="dropdown-item" href="#">Терминалы</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Переводы по картам</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Новости</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Контакты</a>
+                        </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
